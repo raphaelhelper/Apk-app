1c82ad7c00d751289cd0afeef60ac77b1383a28e
raphaelhelper <lebaquico31999@gmail.com>
2026-09-24 22:06:38 +0700

    Add documentfile dependency to build.gradle.kts

--- app_build.gradle.kts
+++ app_build.gradle.kts
@@ -37,4 +37,5 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.documentfile:documentfile:1.0.1")
 }
